27c2cc60c2a20b54378844a2fdce3dec4dd85b0f
Kayne Neigherbauer <kayne.n@gmail.com>
2017-04-20 18:31:44 -0500

    stupid commit

--- js/main.js
+++ js/main.js
@@ -4,16 +4,27 @@ function initialize(){
 
 // Creating a function to instantiate the map with Leaflet
 function createMap(){
-  var map = L.map('mapid', {
-    center: [43.0731,-89.4012],
-    zoom: 11
-  });
+    var map = L.map('mapid', {
+        center: [43.0731,-89.4012],
+        zoom: 11
+    });
 
-// Adding the Satellite tilelayer
-  L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-		  maxZoom: 19,
-      attribution: '&copy; <a href="http://www.esri.com/">Esri</a>'
+    // Adding the Satellite tilelayer
+    L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+		maxZoom: 19,
+        attribution: '&copy; <a href="http://www.esri.com/">Esri</a>'
     }).addTo(map);
+    
+    
 };
+
+//function to load geojson data with ajax
+function loadData(map){
+    //load the data via ajax
+    
+    
+};
+
+
 
 $(document).ready(initialize);
